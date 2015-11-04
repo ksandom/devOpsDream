ee2c1a313b6aeb338eaabd4b784c227896a2cf40
Kevin Sandom <kevinsandom@yahoo.co.uk>
2015-11-04 08:27:20 +0000

    DevOpsDreamUpdater: Cleaned up debugging output.

--- automation/Docker/DevOpsDreamUpdater/internal/updaterService.sh
+++ automation/Docker/DevOpsDreamUpdater/internal/updaterService.sh
@@ -3,8 +3,7 @@
 intervalSeconds=600
 verbosity=1
 
-set -x
-
+echo "Setting git config"
 git config --global user.email "you@example.com"
 git config --global user.name "devOpsDreamUpdater service"
 git config --global push.default matching
@@ -14,15 +13,18 @@ while true;do
 	repos=`find */.git/config -maxdepth 1 -user \`whoami\` | cut -d/ -f 1`
 	for repo in $repos;do
 		cd ~/".achel/repos/$repo"
+		echo "Pulling $repo."
 		git stash # TODO ditch any uncommitted changes, rather than stash
 		git pull
 	done
 	
+	echo "Getting everything."
 	d -v --awsGetAll
 	
 	cd ~/.achel/repos
 	for repo in $repos;do
 		cd ~/".achel/repos/$repo"
+		echo "Commiting and changes to $repo."
 		git add .
 		git commit -m 'devOpsDreamUpdater: Data commit.'
 		git pull
